10bb73a333ff34de22fd9f08dd7a2a32163313ba
Mubzz09 <143597850+DevMubzly@users.noreply.github.com>
2025-09-16 20:45:08 +0300

    fixed the theming to default to only light mode

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark" storageKey="fortress-theme">
+    <ThemeProvider defaultTheme="light" storageKey="fortress-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
